f58c88ca5734284e4c663713a14ec519bc3e7a07
wofockham <certainthings@gmail.com>
2021-09-24 14:34:47 +1000

    this and prototypes

--- 01-javascript/factories/js/factories.js
+++ 01-javascript/factories/js/factories.js
@@ -1,5 +1,3 @@
-
-
 // Factory (unofficial standard)
 
 const catFactory = function (n, a, f) {
@@ -8,7 +6,10 @@ const catFactory = function (n, a, f) {
     age: a,
     furColor: f,
     species: 'feline',
-    carnivore: true
+    carnivore: true,
+    meow: function () {
+      console.log('Meowww, my name is ' + this.name); // this refers to the current object
+    }
   };
 };
 
@@ -24,7 +25,25 @@ for (let i = 0; i < cuteCats.length; i++) {
   console.log(`${ cuteCats[i].name } is ${ cuteCats[i].furColor }`);
 }
 
+// Prototypes
+// Historically interesting but not essential
+// Especially now we have classes
+// Probably don't use these.
 
+// Constructor (similar to a factory but with more magic)
+const Cat = function (n, a, f) {
+  // No need to create an object: an object is already there for you and it's called `this`.
+  this.name = n;
+  this.age = a;
+  this.furColor = f;
+  // No need to `return` anything, `this` gets returned automagically.
+};
 
+const cooper = new Cat('Cooper', 5, 'striped');
+const audrey = new Cat('Audrey', 5, 'striped');
 
-// Prototypes (official but most people hate it)
+// The main "advantage"
+// You can extend the prototype AFTER you create your instances.
+
+Cat.prototype.species = 'feline';
+console.log( audrey.species ); // feline
