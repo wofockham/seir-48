8ccf78b4d57c5071757bbddceb8eb32ea8d46c29
wofockham <certainthings@gmail.com>
2021-10-01 15:03:50 +1000

    jquery playground concluded

--- 04-jquery/playground/js/playground.js
+++ 04-jquery/playground/js/playground.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
   $('#name').on('keyup', function () {
     const name = $('#name').val();
     $('header h1').text('Welcome ' + name);
-  });
-
-  $('#name').on('focus', function () {
+  }).on('focus', function () {
     $('header').addClass('active');
+  }).on('blur', function () {
+    $('header').removeClass('active');
   });
 
-  $('#name').on('blur', function () {
-    $('header').removeClass('active');
-  })
+  $(window).on('mousemove', function (event) {
+    const opacity = event.clientY / window.innerHeight;
+    $('img').css('opacity', opacity); // 0..1.
+  });
 });
