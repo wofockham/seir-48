5f6d7cb7b66c7c436569732cc1cd8656c67b640e
wofockham <certainthings@gmail.com>
2021-10-01 15:03:28 +1000

    funText

--- 04-jquery/madlibs/js/madlibs.js
+++ 04-jquery/madlibs/js/madlibs.js
@@ -1,4 +1,4 @@
-const makeMadLib = function () {
+const makeMadLib = function () { // Global scope
   const noun = $('#noun').val();
   const adjective = $('#adjective').val();
   const name = $('#person').val();
@@ -7,4 +7,10 @@ const makeMadLib = function () {
   $('#story').prepend(`<p>${ story }</p>`);
 };
 
-$('#lib-button').on('click', makeMadLib);
+// Javascript is asynchronous
+$(document).ready(function () {
+  $('#lib-button').on('click', makeMadLib);
+
+  $('h1').funText(500, 'reverseCandy');
+  $('li').funText(100, 'rainbow');
+});
