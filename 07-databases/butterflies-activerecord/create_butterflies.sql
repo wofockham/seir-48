62474c3cbc6cc8d90d968754ce14b0aec392f34c
wofockham <certainthings@gmail.com>
2021-10-15 12:55:21 +1100

    active record butterflies

---
+++ 07-databases/butterflies-activerecord/create_butterflies.sql
@@ -0,0 +1,11 @@
+-- strongly recommend plural table names
+CREATE TABLE butterflies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  family TEXT,
+  image TEXT -- URL for a photo of a butterfly
+);
+
+-- seed data
+INSERT INTO butterflies (name, family) VALUES ('Monarch', 'Nymphalidae');
+INSERT INTO butterflies (name, family) VALUES ('Ulysses', 'Blue');
