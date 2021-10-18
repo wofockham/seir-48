add64444be26ede80bf8219e28c5b47f14c1f26f
wofockham <certainthings@gmail.com>
2021-10-18 15:12:22 +1100

    rails intro part ii

--- 08-rails/rails-intro/config/routes.rb
+++ 08-rails/rails-intro/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get '/hello' => 'pages#hello' # 'controller#action'
   get '/goodbye' => 'pages#goodbye'
+  get '/funny' => 'pages#funny'
+
+  get '/auto/:color' => 'auto#color' # /auto/purple
+  get '/auto/:hp/:torque' => 'auto#engine'
+
+  get '/calc/:x/:y/:operator' => 'calculator#calculate'
 end
