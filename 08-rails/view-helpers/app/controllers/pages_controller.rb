49863aff640d36b9fee145ff7e24447d23f269df
wofockham <certainthings@gmail.com>
2021-10-19 12:11:33 +1100

    url helpers

--- 08-rails/view-helpers/app/controllers/pages_controller.rb
+++ 08-rails/view-helpers/app/controllers/pages_controller.rb
@@ -17,4 +17,7 @@ class PagesController < ApplicationController
 
   def assets
   end
+
+  def urls
+  end
 end
--- 08-rails/view-helpers/config/routes.rb
+++ 08-rails/view-helpers/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
 
   get '/numbers' => 'pages#numbers'
-  get '/text' => 'pages#text'
+  get '/kfctext' => 'pages#text', :as => :text
   get '/assets' => 'pages#assets'
   get '/url' => 'pages#urls'
 end
