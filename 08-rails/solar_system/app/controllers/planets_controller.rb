d30b4700fb5dbdbdb075c4094b0230e927ba35b1
wofockham <certainthings@gmail.com>
2021-10-19 16:33:23 +1100

    planets CRUD

---
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -0,0 +1,39 @@
+class PlanetsController < ApplicationController
+  def index
+    @planets = Planet.all
+  end
+
+  def show
+    @planet = Planet.find params[:id]
+  end
+
+  def new
+  end
+
+  def create
+    planet = Planet.create :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
+    redirect_to planet_path(planet.id) # show page: /planets/23
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.name = params[:name]
+    planet.image = params[:image]
+    planet.orbit = params[:orbit]
+    planet.diameter = params[:diameter]
+    planet.mass = params[:mass]
+    planet.moons = params[:moons]
+    planet.save
+    redirect_to planet_path(planet.id)
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy # Alderaaan!
+    redirect_to planets_path
+  end
+end
