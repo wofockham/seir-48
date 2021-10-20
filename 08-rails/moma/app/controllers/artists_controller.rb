1e2c4ce4b5f4e134fc5fd8caa9c2ad0bce4f7790
wofockham <certainthings@gmail.com>
2021-10-20 12:58:30 +1100

    artists new, create and DESTROY KEN DONE

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -2,4 +2,28 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find params[:id]
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist # show page
+  end
+
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+  def artist_params # strong params (see docs)
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
+  end
 end
