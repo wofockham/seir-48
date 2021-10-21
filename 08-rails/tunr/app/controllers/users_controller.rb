32af39411f943fb707b320861cd6c560029a42a1
wofockham <certainthings@gmail.com>
2021-10-21 15:08:49 +1100

    sign up and validations

---
+++ 08-rails/tunr/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path # You can redirect wherever you like.
+    else
+      render :new # show them the form again and let them have another go.
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_000248) do
+ActiveRecord::Schema.define(version: 2021_10_21_014119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_10_21_000248) do
     t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
