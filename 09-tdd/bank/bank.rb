36305be864023a4a3947c8d263c2474eda0e4837
wofockham <certainthings@gmail.com>
2021-11-01 12:00:39 +1100

    for alex

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -9,4 +9,16 @@ class Bank
   def create_account(account_name, starting_deposit)
     @accounts[account_name] = starting_deposit
   end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount
+  end
+
+  def withdraw(account_name, amount)
+    @accounts[account_name] -= amount
+  end
+
+  def balance(account_name)
+    @accounts[account_name] # implicit return
+  end
 end
--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,23 +1,52 @@
 require_relative '../bank'
 
 describe Bank do
+  let (:bank) { Bank.new 'TDD Bank' }
+
   describe '.new' do
     it 'creates a new bank object' do
-      bank = Bank.new 'TDD Bank'
       expect(bank).to_not be nil
     end
 
     it 'assigns a bank a name' do
-      bank = Bank.new 'TDD Bank'
       expect(bank.name).to eq 'TDD Bank'
     end
   end
 
   describe '#create_account' do
     it 'creates an account for a particular user' do
-      bank = Bank.new 'TDD Bank'
       bank.create_account 'Craigsy', 7
       expect(bank.accounts['Craigsy']).to eq 7
+    end
+  end
+
+  describe '#deposit' do
+    it 'deposits money into a particular account' do
+      bank.create_account 'Jonesy', 200
+      bank.deposit 'Jonesy', 300
+      expect(bank.accounts['Jonesy']).to eq 200+300
+    end
+  end
+
+  describe '#withdraw' do
+    it 'withdraws an amount from a particular account' do
+      bank.create_account 'Mad Robby', 1000
+      bank.withdraw 'Mad Robby', 800
+      expect(bank.accounts['Mad Robby']).to eq 200
+    end
+
+    it 'ignores withdrawals that exceed the balance' do
+      bank.create_account 'Evil Pat', 1
+      bank.withdraw 'Evil Pat', 100_000_000_000
+      expect(bank.balance('Evil Pat')).to eq 1
+    end
+  end
+
+  describe '#balance' do
+    it 'returns the balance for some particular account' do
+      bank.create_account 'Xavier', 5000
+      bal = bank.balance 'Xavier'
+      expect(bal).to eq 5000
     end
   end
 end
