d605801fbd8cab4ab8101a208bba832b71ce802c
wofockham <certainthings@gmail.com>
2021-11-22 17:47:17 +1100

    express stocks solution part i

---
+++ 15-node/stocks/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs');
+const stockfinder = require('stockfinder');
+
+const server = express();
+server.set('view-engine', ejs);
+const PORT = 1337; // could be any number higher than 1024
+const APIKEY = 'pk_16a849fd637243a79fff90fa4d42bc5d';
+
+server.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+server.get('/info', (req, res) => {
+  stockfinder.getStock({ticker: req.query.symbol, apiKey: APIKEY}).then((result) => {
+    console.log(result);
+    res.render('info.ejs', { price: result.close, symbol: req.query.symbol });
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running: http://localhost:${ PORT }`));
