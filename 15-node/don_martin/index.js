58504921777dfaffa26e5031f49ec7b23c15d715
wofockham <certainthings@gmail.com>
2021-11-24 10:54:28 +1100

    don martin sound effects scraper

---
+++ 15-node/don_martin/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const sourceURL = 'https://www.madcoversite.com/dmd-alphabetical.html';
+
+axios.get(sourceURL).then((response) => {
+  const html = response.data;
+  const $ = cheerio.load(html); // see docs
+  const $rows =  $('table table tr:nth-child(n+2)'); // I spent actually hours on this.
+
+  const entries = [];
+
+  $rows.each(function () { // need `this` so we can't use an arrow function
+    const $cells = $('td', this);
+
+    const entry = {
+      sound: $cells.eq(0).text(),
+      description: $cells.eq(1).text(),
+      source: $cells.eq(2).text(),
+      title: $cells.eq(3).text()
+    };
+
+    entries.push(entry);
+  });
+
+  // See docs for indentation in JSON.stringify
+  fs.writeFile('./martin.json', JSON.stringify(entries, null, 2), () => {
+    console.log(`martin.json saved: ${ entries.length } entries`);
+  });
+});
