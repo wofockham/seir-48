eaa5296dd952cba571d6e6c5847294ccf2ed4fe1
wofockham <certainthings@gmail.com>
2021-11-25 11:14:47 +1100

    binary search for angeline

---
+++ 12-comp-sci/search-algorithms/searches.js
@@ -0,0 +1,44 @@
+function binarySearch (haystack, needle) {
+  let start = 0;
+  let end = haystack.length - 1;
+  let midpoint = Math.floor((start + end) / 2); // There is a serious bug on this line.
+
+  // while needle is not found AND there are still haystack elements to consider
+  while (haystack[midpoint] !== needle && start < end) {
+    if (needle < haystack[midpoint]) {
+      end = midpoint - 1;
+    } else {
+      start = midpoint + 1;
+    }
+    midpoint = Math.floor((start + end) / 2);
+  }
+
+  return (needle === haystack[midpoint]) ? midpoint : -1;
+};
+
+const hello = function (name=prompt("Who are you?")) {
+  console.log('Hello there', name)
+};
+
+function recursiveBinarySearch (haystack,
+                                needle,
+                                start=0,
+                                end=haystack.length-1,
+                                midpoint=Math.floor((start+end)/2)) {
+
+  if (haystack[midpoint] === needle) return midpoint; // base case: success
+  if (start >= end) return -1; // base case: failure
+
+  if (needle < haystack[midpoint]) {
+    end = midpoint - 1;
+  } else {
+    start = midpoint + 1;
+  }
+
+  return recursiveBinarySearch(haystack, needle, start, end);
+}
+
+module.exports = {
+    binarySearch,
+    recursiveBinarySearch
+}
