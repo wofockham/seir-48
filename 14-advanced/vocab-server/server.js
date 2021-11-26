70c63ced0497b8034b669724e76c60f020484218
wofockham <certainthings@gmail.com>
2021-11-26 15:39:41 +1100

    vocab server part ii

--- 14-advanced/vocab-server/server.js
+++ 14-advanced/vocab-server/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Vocab = require('./api/models/vocabModel'); // Make the Vocab model globally available.
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise; // Use the built-in JS Promise library for promises.
+
+mongoose.connect(
+  `mongodb+srv://theBlade:${ process.env.MONGOPW }@cluster0.i0cbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
+
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 app.listen(port);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// 404
+app.use((req, res) => {
+  res.status(404).send({ url: `${ req.originalUrl } not found` });
+})
 
 console.log(`Server started on http://localhost:${ port }`);
-  
