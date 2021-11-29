0207d2c4a4bc78884e057d0a4b162870e4034015
wofockham <certainthings@gmail.com>
2021-11-29 15:36:26 +1100

    vocab word-form edit WIP

---
+++ 14-advanced/vocab-client/src/components/WordForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form action="#" @submit.prevent="onSubmit">
+    <p v-if="errorsPresent" class="error">Please fill out both fields!</p>
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="ui germany flag"></i> German
+      </div>
+      <input type="text" placeholder="Enter German word..." v-model="word.german" />
+    </div>
+    <div class="ui labeled input fluid">
+      <div class="ui label">
+        <i class="ui united kingdom flag"></i> English
+      </div>
+      <input type="text" placeholder="Enter English word..." v-model="word.english" />
+    </div>
+
+    <button class="positive ui button">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'word-form',
+  props: {
+    word: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      errorsPresent: false
+    }
+  },
+  methods: {
+    onSubmit: function () {
+      if (this.word.english === '' || this.word.german === '') {
+        this.errorsPresent = true;
+      } else {
+        console.log('TODO: save the updates to the server');
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.error {
+  color: red;
+}
+</style>
---
+++ 14-advanced/vocab-client/src/views/Edit.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Edit Word</h1>
+    <word-form :word=this.word></word-form>
+  </div>
+</template>
+
+<script>
+import WordForm from '../components/WordForm.vue';
+import { api } from '../helpers/helpers';
+
+export default {
+  name: 'edit',
+  components: {
+    'word-form': WordForm
+  },
+  data() {
+    return {
+      word: {}
+    }
+  },
+  async mounted() {
+    this.word = await api.getWord(this.$route.params.id);
+  }
+}
+</script>
