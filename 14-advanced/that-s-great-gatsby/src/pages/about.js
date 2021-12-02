0f624a357808e549a858f6921feb7038fbfb404b
wofockham <certainthings@gmail.com>
2021-12-02 15:06:48 +1100

    that's great, gatsby

---
+++ 14-advanced/that-s-great-gatsby/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>I'm just some guy, you know?</p>
+    </Layout>
+  )
+};
+
+export default About;
---
+++ 14-advanced/that-s-great-gatsby/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>That's Great, Gatsby</p>
+    </Layout>
+  )
+};
+
+export default IndexPage;
